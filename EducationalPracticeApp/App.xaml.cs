ddce2a7c10acc63fad833ee6a3c8320382ce6076
broGideon <turuncev70@gmail.com>
2025-01-25 02:04:35 +0300

    fix

--- EducationalPracticeApp/App.xaml.cs
+++ EducationalPracticeApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
 
 namespace EducationalPracticeApp;
 
@@ -7,4 +8,11 @@ namespace EducationalPracticeApp;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ru-RU");
+        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("ru-RU");
+    }
 }
